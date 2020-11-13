5264f1a1a7b872b0d2b4c90727976220aa543790
Jacinta Olonilua <jacintaolonilua@Jacintas-MBP.home>
2020-11-12 20:00:07 -0500

    on task 4

--- src/CardEditor.js
+++ src/CardEditor.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import './CardEditor.css';
 
 class CardEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { front: '', back: ''};
+    }
+
+    addCard = () => {
+        this.props.addCard(this.state);
+        this.setState({ front: '', back: ''});
+    };
+
+    deleteCard = index => this.props.deleteCard(index);
+
+    handleChange = event => 
+        this.setState({ [event.target.name]: event.target.value });
     render() {
         const cards = this.props.cards.map((card, index) => {
             return (
@@ -8,7 +23,7 @@ class CardEditor extends React.Component {
                     <td>{card.front}</td>
                     <td>{card.back}</td>
                     <td>
-                        <button>Delete card</button>
+                        <button onClick= {() =>{this.deleteCard(index)}}>Delete card</button>
                     </td>
                 </tr>
             );
@@ -27,6 +42,21 @@ class CardEditor extends React.Component {
                     </thead>
                     <tbody>{cards}</tbody>
                 </table>
+                <br/>
+                <input 
+                    name="front"
+                    onChange={this.handleChange} 
+                    placeholder="Front of card" 
+                    value={this.state.front}/>
+                <input 
+                    name="back"
+                    onChange={this.handleChange} 
+                    placeholder="Back of card" 
+                    value={this.state.back}
+                />
+                <button onClick={this.addCard}>Add card</button> 
+                <hr/>
+                <button onClick={this.props.switchMode}>Go to card viewer</button>
             </div>
         )
     }
@@ -34,4 +64,3 @@ class CardEditor extends React.Component {
 
 export default CardEditor;
 
-// stopped at 7:43
